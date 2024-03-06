f66db37ab8c4adf4c36affd6c5f3d62e55663a6e
adem-mekonnen <adem.mekonin@wku.edu.et>
2024-03-07 00:57:17 +0300

    cors

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -15,7 +6,11 @@ dotenv.config();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 const app = express();
-app.use(cors({ origin: true }));
+app.use(
+  cors({
+    origin: "https://amazon-clone-by-adem.netlify.app",
+  })
+);
 app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).json({ message: "succes" });
