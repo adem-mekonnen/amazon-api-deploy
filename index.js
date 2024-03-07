0c9fac6e05a09b01cfa5b6f76914802697cecfe7
adem-mekonnen <adem.mekonin@wku.edu.et>
2024-03-07 17:08:22 +0300

    strip corrections

--- index.js
+++ index.js
@@ -6,24 +6,29 @@ dotenv.config();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: true }));
 app.use(express.json());
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "succes" });
+  res.status(200).json({ message: "success" });
 });
 app.post("/payment/create", async (req, res) => {
   const total = req.query.total;
   if (total > 0) {
-    //console.log("recieved total", total);
-    //res.send(total);
-    const paymentIntent = await stripe.paymentIntent.create({
-      amount: total,
-      currency: "usd",
-    });
-    console.log(paymentIntent);
-    res.status(201).json({
-      clientSecret: paymentIntent.client_secret,
-    });
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,
+        currency: "usd",
+      });
+      console.log(paymentIntent);
+      res.status(201).json({
+        clientSecret: paymentIntent.client_secret,
+      });
+    } catch (error) {
+      console.error("Error creating payment intent:", error);
+      res.status(500).json({
+        message: "Error creating payment intent",
+      });
+    }
   } else {
     res.status(403).json({
       message: "payment must be greater than zero",
@@ -32,15 +37,5 @@ app.post("/payment/create", async (req, res) => {
 });
 app.listen(3500, (err) => {
   if (err) throw err;
-  console.log("amazon server runnig on port ,http://localhost:3500");
+  console.log("amazon server running on port, http://localhost:3500");
 });
-//exports.api = onRequest(app);
-// Your routes and logic using Express go here...
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
